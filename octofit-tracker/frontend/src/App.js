6e2091dafeb9cc1e4a664a0f404cc566ef7df7de
George Owino <fuhrer.go@gmail.com>
2025-11-13 16:28:49 +0000

    Commit frontend components and routing

--- octofit-tracker/frontend/src/App.js
+++ octofit-tracker/frontend/src/App.js
@@ -1,23 +1,37 @@
-import logo from './logo.svg';
 import './App.css';
+import { NavLink, Routes, Route } from 'react-router-dom';
+import Activities from './components/Activities';
+import Users from './components/Users';
+import Teams from './components/Teams';
+import Workouts from './components/Workouts';
+import Leaderboard from './components/Leaderboard';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="App container mt-4">
+      <nav className="navbar navbar-expand-lg navbar-light bg-light mb-4">
+        <div className="container-fluid">
+          <a className="navbar-brand" href="#">OctoFit</a>
+          <div className="collapse navbar-collapse">
+            <ul className="navbar-nav me-auto mb-2 mb-lg-0">
+              <li className="nav-item"><NavLink className="nav-link" to="/activities">Activities</NavLink></li>
+              <li className="nav-item"><NavLink className="nav-link" to="/users">Users</NavLink></li>
+              <li className="nav-item"><NavLink className="nav-link" to="/teams">Teams</NavLink></li>
+              <li className="nav-item"><NavLink className="nav-link" to="/workouts">Workouts</NavLink></li>
+              <li className="nav-item"><NavLink className="nav-link" to="/leaderboard">Leaderboard</NavLink></li>
+            </ul>
+          </div>
+        </div>
+      </nav>
+
+      <Routes>
+        <Route path="/" element={<Activities />} />
+        <Route path="/activities" element={<Activities />} />
+        <Route path="/users" element={<Users />} />
+        <Route path="/teams" element={<Teams />} />
+        <Route path="/workouts" element={<Workouts />} />
+        <Route path="/leaderboard" element={<Leaderboard />} />
+      </Routes>
     </div>
   );
 }
